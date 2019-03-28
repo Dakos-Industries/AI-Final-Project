d61126a97f3e7dc2b078864f0d6ab05618b8da5e
Spiros Mavoridakos <spirosmavroidakos@gmail.com>
2019-03-27 23:14:45 -0400

    Initial AI complete. Can defeat random player.

--- src/student_player/MyTools.java
+++ src/student_player/MyTools.java
@@ -1,7 +1,203 @@
 package student_player;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+import com.sun.corba.se.spi.orbutil.fsm.Guard.Result;
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import boardgame.Board;
+import boardgame.Move;
+import pentago_swap.PentagoBoardState;
+import pentago_swap.PentagoMove;
+import sun.net.www.content.audio.x_aiff;
+
 public class MyTools {
     public static double getSomething() {
         return Math.random();
     }
+    
+    public static Move GetBestMove(Node current)
+    {
+    	ArrayList<PentagoMove> moves = current.bs.getAllLegalMoves();
+    	
+    	PentagoMove candidateMove = moves.get(0);
+    	double bestRatio = 0;
+    	int attempt = 0;
+    	for(PentagoMove move : moves)
+    	{
+    		attempt++;
+    		SimulationResult result = CountingDFS(current, move, new SimulationResult());
+    		if (result.GetWinLossRatio() > bestRatio)
+    		{
+    			bestRatio = result.GetWinLossRatio();
+    			candidateMove = move;
+    			System.out.println("Attemp " + attempt + "| Ratio " + bestRatio + "|Games played " + (result.Losses + result.Wins + result.Draws));
+    		}
+    	}   	
+    	return candidateMove;
+    }
+    
+    private static SimulationResult CountingDFS(Node current, PentagoMove move, SimulationResult result)
+    {
+		if(result.GetTotalTrials() >= 175)
+		{
+			result.ContinueSim = false;
+			return result;
+		}
+		
+    	Node next = new Node();
+		next.bs = (PentagoBoardState) current.bs.clone();
+		int maxPlayer = next.bs.getTurnPlayer();
+		next.bs.processMove(move);
+		
+		int minPlayer = next.bs.getTurnPlayer();
+		if(next.bs.getWinner() == maxPlayer)
+		{
+			result.Wins++;
+			return result;
+		} 
+		else if (next.bs.getWinner() == minPlayer)
+		{
+			result.Losses++;
+			return result;
+		}
+		else if (next.bs.getWinner() == Board.DRAW) {
+			result.Draws++;
+			return result;
+		}
+		
+		//simulate enemy
+		Node enemy = new Node();
+		enemy.bs = (PentagoBoardState) next.bs.clone();
+		Move move2 = enemy.bs.getRandomMove();
+		enemy.bs.processMove((PentagoMove)move2);
+
+		if(enemy.bs.getWinner() == maxPlayer)
+		{
+			result.Wins++;
+			return result;
+		} 
+		else if (enemy.bs.getWinner() == minPlayer)
+		{
+			result.Losses++;
+			return result;
+		}
+		else if (enemy.bs.getWinner() == Board.DRAW) {
+			result.Draws++;
+			return result;
+		}
+		ArrayList<PentagoMove> moves = enemy.bs.getAllLegalMoves();
+    	
+    	for(PentagoMove pentagoMove : moves)
+    	{
+    		SimulationResult simulationResult = CountingDFS(enemy, pentagoMove, result);
+    		if(!simulationResult.ContinueSim)
+    		{
+    			return result;
+    		}
+    	}
+		return result;
+    }
+
+    public static PentagoMove MonteCarlo(Node current)
+    {
+    	ArrayList<PentagoMove> moves = current.bs.getAllLegalMoves();
+    	CandidateMove[] candidates = new CandidateMove[5];
+    	
+    	int i = 0;
+    	for(PentagoMove move : moves)
+    	{
+    		SimulationResult result = CountingDFS(current, move, new SimulationResult());
+    		if(i < 5)
+    		{
+    			candidates[i] = new CandidateMove(move, result.GetWinLossRatio(), result.GetHeuristicScore());
+    			if (i == 4) {
+    				Arrays.sort(candidates);
+				}
+    			i++;
+    			continue;
+    		}
+    		
+    		if (result.GetHeuristicScore() > candidates[0].HeuristicScore)
+    		{
+    			candidates[0] = new CandidateMove(move, result.GetWinLossRatio(), result.GetHeuristicScore());
+    		}
+    	}
+    	
+    	PentagoMove bestMove = candidates[0].Move;
+    	for (int k = 0; k < candidates.length; k++)
+    	{
+    		Node nextLevel = new Node();
+    		nextLevel.bs = (PentagoBoardState) current.bs.clone();
+    		nextLevel.bs.processMove(candidates[k].Move);
+    		
+    	}
+    	
+    	return bestMove;
+    }
+
+
+
+    private static SimulationResult InitializeMC(Node current, PentagoMove moveA, PentagoMove moveB)
+    {
+    	SimulationResult result = new SimulationResult();
+		Node next = new Node();
+		next.bs = (PentagoBoardState) current.bs.clone();
+		int maxPlayer = next.bs.getTurnPlayer();
+		next.bs.processMove(moveA);
+		
+		int minPlayer = next.bs.getTurnPlayer();
+		if(next.bs.getWinner() == maxPlayer)
+		{
+			result.Wins = 30;
+			return result;
+		} 
+		else if (next.bs.getWinner() == minPlayer)
+		{
+			result.Losses = 30;
+			return result;
+		}
+		else if (next.bs.getWinner() == Board.DRAW) {
+			result.Draws = 30;
+			return result;
+		}
+		
+		//simulate enemy
+		Node enemy = new Node();
+		enemy.bs = (PentagoBoardState) next.bs.clone();
+		
+		// Now we want to run all the enemy moves
+		ArrayList<PentagoMove> moves = enemy.bs.getAllLegalMoves();
+    	for(PentagoMove pentagoMove : moves)
+    	{
+    		Move move2 = enemy.bs.getRandomMove();
+    		enemy.bs.processMove((PentagoMove)move2);
+
+    		if(enemy.bs.getWinner() == maxPlayer)
+    		{
+    			result.Wins++;
+    		} 
+    		else if (enemy.bs.getWinner() == minPlayer)
+    		{
+    			result.Losses++;
+    		}
+    		else if (enemy.bs.getWinner() == Board.DRAW) {
+    			result.Draws++;
+    		}
+    		
+    		SimulationResult simulationResult = CountingDFS(enemy, pentagoMove, result);
+    		if(!simulationResult.ContinueSim)
+    		{
+    			return result;
+    		}
+    	}
+    	
+		return result;
+    }
+
 }
--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -1,9 +1,15 @@
 package student_player;
 
+import java.util.ArrayList;
+import java.util.concurrent.RecursiveAction;
+
+import com.sun.xml.internal.org.jvnet.fastinfoset.FastInfosetSerializer;
+
 import boardgame.Move;
 
 import pentago_swap.PentagoPlayer;
 import pentago_swap.PentagoBoardState;
+import pentago_swap.PentagoMove;
 
 /** A player file submitted by a student. */
 public class StudentPlayer extends PentagoPlayer {
@@ -14,7 +20,7 @@ public class StudentPlayer extends PentagoPlayer {
      * associate you with your agent. The constructor should do nothing else.
      */
     public StudentPlayer() {
-        super("xxxxxxxxx");
+        super("Zero System");
     }
 
     /**
@@ -22,15 +28,75 @@ public class StudentPlayer extends PentagoPlayer {
      * object contains the current state of the game, which your agent must use to
      * make decisions.
      */
-    public Move chooseMove(PentagoBoardState boardState) {
-        // You probably will make separate functions in MyTools.
-        // For example, maybe you'll need to load some pre-processed best opening
-        // strategies...
-        MyTools.getSomething();
-
-        Move myMove = boardState.getRandomMove();
+    public Move chooseMove(PentagoBoardState boardState) {        
+        Node root = new Node();
+        root.bs = (PentagoBoardState) boardState.clone();
+        
+        Move attempt = MyTools.GetBestMove(root);
 
         // Return your move to be processed by the server.
-        return myMove;
+        return attempt;
     }
+    
+    private Move GetBestMove(Node current)
+    {
+    	ArrayList<PentagoMove> moves = current.bs.getAllLegalMoves();
+    	
+    	for(PentagoMove move : moves)
+    	{
+    		if (this.SimpleDFS(current, move))
+    		{
+    			return move;
+    		}
+    	}
+    	
+    	
+    	return moves.get(0);
+    }
+    
+    private Boolean SimpleDFS(Node current, PentagoMove move)
+    {
+    	Node next = new Node();
+		next.bs = (PentagoBoardState) current.bs.clone();
+		int maxPlayer = next.bs.getTurnPlayer();
+		next.bs.processMove(move);
+		
+		if(next.bs.getWinner() == maxPlayer)
+		{
+			return true;
+		}
+		
+		//simulate enemy
+		Node enemy = new Node();
+		enemy.bs = (PentagoBoardState) next.bs.clone();
+		int minPlayer = next.bs.getTurnPlayer();
+		PentagoMove move2 = (PentagoMove) enemy.bs.getRandomMove();
+		enemy.bs.processMove(move2);
+		
+		if(next.bs.getWinner() == minPlayer)
+		{
+			return false;
+		}
+		
+		//draw
+		if (enemy.bs.getTurnNumber() >= 36)
+		{
+			return false;
+		}
+		
+		ArrayList<PentagoMove> moves = enemy.bs.getAllLegalMoves();
+    	
+    	for(PentagoMove m : moves)
+    	{
+    		if (this.SimpleDFS(enemy, m))
+    		{
+    			return true;
+    		}
+    	}
+    	
+    	// no move yields victory
+    	return false;
+		
+    }
+    
 }
